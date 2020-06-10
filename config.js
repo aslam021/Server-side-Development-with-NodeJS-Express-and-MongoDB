b3e891f4c641f6df15a50ed5143880eb8dc9e282
aslam021 <muhammadh.aslamm@gmail.com>
2020-06-10 19:41:10 +0530

    fb OAuth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        'appId': '617099545679659',
+        'appSecret' : '437a3a009c413b1d37ddfda490128f5e'
+    }
 }
